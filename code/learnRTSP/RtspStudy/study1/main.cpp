9bd02abd490c2e4483daed2bcec14db2f27ad325
ahwi <jyws66@qq.com>
2023-07-18 22:59:47 +0800

    完善第一节的代码

--- code/learnRTSP/RtspStudy/study1/main.cpp
+++ code/learnRTSP/RtspStudy/study1/main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <winsock2.h>  // windows socket 2 api 头文件 
 #include <ws2tcpip.h>  // windows socket 2 TCP/IP 协议套接字的专用头文件
+#include <time.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define SERVER_PORT 7899
+#define SERVER_PORT 8554
 
 SOCKET createTcpSocket() 
 {
@@ -49,6 +50,110 @@ SOCKET acceptClient(SOCKET serverSock, char *ip, int *port)
 	*port = ntohs(clientAddr.sin_port);
 
 	return clientfd;
+}
+
+static int handleCmd_OPTIONS(char *sendBuf, const int &CSeq) 
+{
+	sprintf(sendBuf, 
+		"Response: RTSP/1.0 200 OK\r\n"
+		"CSeq: %d\r\n"
+		"Public: OPTIONS,DESCRIBE,SETUP,PLAY\r\n"
+		"\r\n", CSeq
+	);
+	return 0;
+}
+
+static int handleCmd_DESCRIBE(char *sendBuf, const int &CSeq, const char *url) 
+{
+	char ip[40];
+	sscanf(url, "rtsp://%[^:]:", ip);
+
+	char sdp[400];
+	sprintf(sdp, 
+		"v=0\r\n"
+		"o=-%ld 1 IN IP4 %s\r\n"
+		"t=0 0\r\n"
+		"a=control:*\r\n"
+		"m=video 0 RTP/AVP 96\r\n"
+		"a=rtmap:96 H264/90000\r\n"
+		"a=control:track0\r\n", time(NULL), ip
+	);
+
+
+	sprintf(sendBuf, 
+		"Response: RTSP/1.0 200 OK\r\n"
+		"CSeq: %d\r\n"
+		"Content-Base:%s\r\n"
+		"Content-type:application/sdp"
+		"Content-lenth:%zu\r\n\r\n"
+		"%s",
+		CSeq,
+		url,
+		strlen(sdp),
+		sdp
+	);
+	return 0;
+}
+
+void doClient(SOCKET clientSockFd) 
+{
+	char method[40];
+	char url[200];
+	char version[40];
+
+	int clientRtpPort = 0, clientRtspPort = 0;
+	char *recvBuf = (char *)malloc(10000);
+	char *sendBuf = (char *)malloc(10000);
+	int CSeq = 0;
+
+	while (true) {
+		int recvLen = 0;
+		recvLen = recv(clientSockFd, recvBuf, 2000, 0);
+		if (recvLen < 0)
+			break;
+
+		const char *sep = "\n";
+		char * line = strtok(recvBuf, sep);
+		while (line) {
+			if (strstr(line, "OPTIONS") ||
+				strstr(line, "DESCRIBE") ||
+				strstr(line, "SETUP") ||
+				strstr(line, "PLAY")) {
+
+				if (sscanf(line, "%s %s %s\r\n", method, url, version) != 3) {
+					// error
+				}
+			}
+			else if (strstr(line, "CSeq")) {
+				if (sscanf(line, "CSeq: %d\r\n", &CSeq) != 1) {
+					// error
+				}
+			}
+			else if (!strncmp(line, "Transport:", strlen("Transport:"))) {
+				if (sscanf(line, "Transport: RTP/AVP;unicast;client_port=%d-%d\r\n\r\n", 
+					&clientRtpPort, &clientRtspPort) != 1) {
+					// error
+					printf("parse Transport error \n");
+				}
+			}
+			line = strtok(NULL, sep);
+		}
+
+		if (!strcmp(method, "OPTIONS")) {
+			if (handleCmd_OPTIONS(sendBuf, CSeq)) {
+				printf("failed to handle options \n");
+				break;
+			}
+		}
+
+		send(clientSockFd, sendBuf, strlen(sendBuf), 0);
+
+		memset(method, 0, sizeof(method));
+		memset(url, 0, sizeof(url));
+		memset(version, 0, sizeof(version));
+
+	
+	}
 }
 
 int main(int argc, char *argv[])
